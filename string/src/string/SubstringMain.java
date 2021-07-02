12120dd38a076447b12c2b14be9d5ba4c77433e8
hhhhyeyoungg <81943817+hhhhyeyoungg@users.noreply.github.com>
2021-07-02 12:31:24 +0900

    substring 테스트

---
+++ string/src/string/SubstringMain.java
@@ -0,0 +1,13 @@
+package string;
+
+public class SubstringMain {
+
+	public static void main(String[] args) {
+		
+		String str = "2021/05/05 09:00";
+		String sub = str.substring(0, 10);
+		System.out.println(sub);
+
+	}
+
+}
